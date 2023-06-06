c0ed880f6307bc1a33a58cf80caf3c8596fdf5dd
Ajith <ajithsahana05@gmail.com>
2023-06-06 17:39:46 +0530

    port number change

--- bloodbank_frontend/src/services/config.js
+++ bloodbank_frontend/src/services/config.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-export const BASE_URL = "http://89.116.30.81:4100/";
+export const BASE_URL = "http://89.116.30.81:8000/";
 
 export const axiosInstance = axios.create({
     baseURL: BASE_URL
