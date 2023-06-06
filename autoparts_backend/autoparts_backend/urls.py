59f77ecc1c6c38f0175d827005c1f8741d349d5b
Ajith <ajithsahana05@gmail.com>
2023-06-06 17:22:04 +0530

    customs

--- autoparts_backend/autoparts_backend/urls.py
+++ autoparts_backend/autoparts_backend/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import include,path
 from customer import views 
 from rest_framework import routers
-from customer.views import EnquiryViewSet
 from krida.views import ProfileSignup
 from django.conf.urls.static import static
 from django.conf import settings
@@ -29,27 +28,27 @@ router = routers.DefaultRouter()
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
-    path('customer/add',views.customer_signup),
+    # path('customer/add',views.customer_signup),
     path('',include('krida.urls')),
-    path('login/api',views.login_api),
-    path('enquiry/add', views.enquiry_create),
-    path('customer/enquiry/list/<int:pk>',views.enquiry_list_by_customer),
-    path('new/enquiry/list',views.new_enquiry_list),
-    path('enquiry/detail/<int:pk>',views.enquiry_detail_by_id),
-    path('new/enquiry/status/update/<int:pk>',views.enquiry_status_update),
-    path('add/supplier',views.supplier_signup),
-    path('view/supplier',views.supplier_id),
-    path('quotation/add', views.quotation_create),
-    path('quotation/view/<int:pk>', views.quotation_list),
-    path('brand/add', views.brand_add),
-    path('car/model/add', views.cmar_model_info),
-    path('car/model/list/<int:pk>', views.car_model_list),
-    path('dashboar/count', views.admin_dashboard_count),
-    path('brand/exists', views.brand_exist),
-    path('model/exists', views.car_model_exist),
-    path('notification/list/<int:pk>', views.notification_list),
-    path('quotation/view/notification', views.QuotationView),
-    path('streaming',views.streaming_API),
+    # path('login/api',views.login_api),
+    # path('enquiry/add', views.enquiry_create),
+    # path('customer/enquiry/list/<int:pk>',views.enquiry_list_by_customer),
+    # path('new/enquiry/list',views.new_enquiry_list),
+    # path('enquiry/detail/<int:pk>',views.enquiry_detail_by_id),
+    # path('new/enquiry/status/update/<int:pk>',views.enquiry_status_update),
+    # path('add/supplier',views.supplier_signup),
+    # path('view/supplier',views.supplier_id),
+    # path('quotation/add', views.quotation_create),
+    # path('quotation/view/<int:pk>', views.quotation_list),
+    # path('brand/add', views.brand_add),
+    # path('car/model/add', views.cmar_model_info),
+    # path('car/model/list/<int:pk>', views.car_model_list),
+    # path('dashboar/count', views.admin_dashboard_count),
+    # path('brand/exists', views.brand_exist),
+    # path('model/exists', views.car_model_exist),
+    # path('notification/list/<int:pk>', views.notification_list),
+    # path('quotation/view/notification', views.QuotationView),
+    # path('streaming',views.streaming_API),
     
 
 ]
